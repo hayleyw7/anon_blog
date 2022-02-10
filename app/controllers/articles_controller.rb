83fefb586ea0b6643452467ac98dc8781e39c2b8
hayleyskyland <hwitherell@skylandanalytics.net>
2022-02-10 15:00:20 -0700

    Set up route and default action

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,11 @@
+class ArticlesController < ApplicationController
+
+  # below is an "action"
+
+  # when an action doesn't explicitly render a view or trigger an http response, rails auto-renders a view that matches the name of the controller & action (app/views/articles/index.html.erb)
+
+  def index
+    
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # maps `GET /articles` requests to index action of `ArticlesController`
+
+  get "/articles", to: "articles#index"
+
 end
