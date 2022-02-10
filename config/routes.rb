5945a7f2d47e0d76e6326dbb5a618d4427d3b3ef
hayleyskyland <hwitherell@skylandanalytics.net>
2022-02-10 15:11:56 -0700

    Change text to Hello World

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   # maps `GET /articles` requests to index action of `ArticlesController`
 
+  # when you go to /articles, take me to method of the controller named `ArticlesController`
+
   get "/articles", to: "articles#index"
 
 end
