00790ee6298cb9cc261b623b2861350d921b7770
hayleyskyland <hwitherell@skylandanalytics.net>
2022-02-10 16:43:51 -0700

    Migrate database

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
-  # maps `GET /articles` requests to index action of `ArticlesController`
+  # map root path of app to appropriate controller & action
 
-  # when you go to /articles, take me to method of the controller named `ArticlesController`
+  root "articles#index"
+
+  # maps `GET /articles` requests to index action of `ArticlesController`
+    # "when i go to /articles, take me to method of the controller"
 
   get "/articles", to: "articles#index"
 
---
+++ db/migrate/20220210233822_create_articles.rb
@@ -0,0 +1,19 @@
+class CreateArticles < ActiveRecord::Migration[7.0]
+  def change
+
+    # specifies how to construct the articles table
+
+    create_table :articles do |t|
+
+      # columns created with `title:string` & `body:text` in larger Terminal cmd
+
+      t.string :title
+      t.text :body
+
+      # 2 columns auto-created: `created_at` & `updated_at ``
+
+      t.timestamps
+    end
+
+  end
+end
