6bf2d027e883d580a856274f1e698b70fb9ce5f1
hayleyskyland <hwitherell@skylandanalytics.net>
2022-02-10 16:54:56 -0700

    Fetch all articles from database

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,9 +2,12 @@ class ArticlesController < ApplicationController
 
   # below is an "action"
 
+  # the default action is `def index`
   # when an action doesn't explicitly render a view or trigger an http response, rails auto-renders a view that matches the name of the controller & action (app/views/articles/index.html.erb)
 
-  def index
+  # this is a specific action, which fetches all articles from database
+
+  @articles = Article.all
     
   end
 
