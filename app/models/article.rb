9f692a14ddef685e71bf8ca483badac6a085e065
hayleyskyland <hwitherell@skylandanalytics.net>
2022-02-11 11:42:06 -0700

    Validate data

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,13 @@
 class Article < ApplicationRecord
+
+# declares title value must be present (via `presence: true`)
+  # default for string - value must be >= 1 char
+
+  validates :title, presence: true
+
+  # declares body value must be present
+    # declares body value must be >= 10 chars (via `length: { minimum: 10 }`)
+
+  validates :body, presence: true, length: { minimum: 10 }
+  
 end
