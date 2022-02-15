e064e257f4894ffa467cfe37dd778f4bee5de589
hayleyskyland <hwitherell@skylandanalytics.net>
2022-02-15 13:09:12 -0700

    Complete commenting feature

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
+# use create method on @article.comments to create/save comment (links comment to  article)
+
 class CommentsController < ApplicationController
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
 end
