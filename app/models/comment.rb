8cd9b5b92f8d94d8a4ff619039b4ea813be86272
hayleyskyland <hwitherell@skylandanalytics.net>
2022-02-15 12:54:05 -0700

    Finish creating model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,8 @@
+# Comment model (similar to Article model)
+
 class Comment < ApplicationRecord
+
+  # set-up "Active Record" association
   belongs_to :article
+
 end
--- db/migrate/20220215194640_create_comments.rb
+++ db/migrate/20220215194640_create_comments.rb
@@ -1,11 +1,22 @@
+# migration to create corresponding dbs table
+
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
+
       t.string :commenter
+
       t.text :body
+
+      # `:references` keyword - special data type that creates new db column w/ model name + `_id` 
+        # to learn more, analyze db/schema.rb after running the migration
+
+       #creates an integer column called `article_id` + index + `foreign_key` constraint pointing to id column of articles table
+
       t.references :article, null: false, foreign_key: true
 
       t.timestamps
+
     end
   end
 end
